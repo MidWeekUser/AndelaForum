9020ecebc83f7a3810fc24aca2030e9b1c50cb0e
Arthur Ngondo <ngondoarthur@gmail.com>
2018-03-02 11:21:25 +0300

    Add users
    [finishes #155649244]

---
+++ users.py
@@ -0,0 +1,60 @@
+class Moderator (CreateNormalUser) :
+	'''Moderators have the added ability to delete comments (to remove trolls'''
+	def __init__(self) :
+		self.user_id = None # int
+		self.username = None # str
+		self.first_name = None # str
+		self.last_name = None # str
+		self.user_level = None # int
+		pass
+	def create_comment (self, comment) :
+		# returns 
+		pass
+	def edit_own_comment (self, new_comment) :
+		# returns 
+		pass
+class Admin (Moderator) :
+	'''admins have the ability to edit or delete any comment.
+admins have the ability to edit or delete any comment.
+admins have the ability to edit or delete any comment.
+admins have the ability to edit or delete any comment.
+admins have the ability to edit or delete any comment.
+admins have the ability to edit or delete any comment.
+'''
+	def __init__(self) :
+		self.user_id = None # int
+		self.username = None # str
+		self.first_name = None # str
+		self.last_name = None # str
+		self.user_level = None # int
+		pass
+	def create_comment (self, comment) :
+		# returns 
+		pass
+	def edit_comments (self, new_comment) :
+		# returns 
+		pass
+	def delete_comments (self) :
+		# returns 
+		pass
+	def create (self) :
+		# returns 
+		pass
+	def  (self) :
+		# returns 
+		pass
+class CreateNormalUser :
+	'''Normal users can only create new comments, and edit the their own comments.'''
+	def __init__(self) :
+		self.user_id = None # int
+		self.username = None # str
+		self.first_name = None # str
+		self.last_name = None # str
+		self.user_level = None # int
+		pass
+	def create_comment (self, comment) :
+		# returns 
+		pass
+	def edit_own_comment (self, new_comment) :
+		# returns 
+		pass
